a8db083da65d3c25a6f1dfad10dd2b8e17a98959
jadentalks <96040145+jadentalks@users.noreply.github.com>
2021-12-15 16:47:10 -0600

    Add files via upload

---
+++ statcounter.js
@@ -0,0 +1,25 @@
+// <script type="text/javascript" src="/path/to/script.js"></script> 
+//Default Statcounter code for Jaden Talks https://jadentalks.github.io/webpages
+
+//Using the script tag to include an external JavaScript file
+//To include an external JavaScript file, we can use the script tag 
+//with the attribute src. You've already used the src attribute when 
+//using images. The value for the src attribute should be the path to 
+//your JavaScript file.
+//<script type="text/javascript" src="path-to-javascript-file.js"></script>
+//This script tag should be included between the <head> tags in your HTML document.
+//http://web.simmons.edu/~grabiner/comm244/weeknine/including-javascript.html
+
+<script type="text/javascript">
+var sc_project=12690453; 
+var sc_invisible=1; 
+var sc_security="552765ac"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics Made Easy -
+Statcounter" href="https://statcounter.com/" target="_blank"><img
+class="statcounter" src="https://c.statcounter.com/12690453/0/552765ac/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
